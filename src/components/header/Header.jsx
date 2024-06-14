65cf2af6a45a7f86f5b7773294a3af84f4e50758
Diana <162273609+DianaBabayeva03@users.noreply.github.com>
2024-06-15 00:54:46 +0400

    Add files via upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,8 +12,24 @@ const Header = () => {
                     <button>Daşınma şərtləri</button>
                     <button>Şəxsi məlumatların qorunması</button>
                 </div>
+                <div className={styles.contact}>
+                  <div className={styles.valyuta}>
+                    <span>USD</span>
+                    <span>AZN</span>
+                  </div>
+                  <div className={styles.number}>
+                    <p>+994 12 525 26 26</p>
+                  </div>
+                  <div className={styles.selects}>
+                    <select name="" id="">
+                      <option value="">az</option>
+                      <option value="">ru</option>
+                    </select>
+                  </div>
+                </div>
             </div>
         </div>
+        <div className={styles.headerBottom}></div>
     </div>
   )
 }
